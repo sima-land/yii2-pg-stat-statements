5bd096aaeb6918025d09d6fc7b8927dc29053d6c
Danil <meshcheryakov@sima-land.ru>
2017-01-17 18:26:16 +0500

    fix

--- src/commands/AggregatePgStatStatementsController.php
+++ src/commands/AggregatePgStatStatementsController.php
@@ -32,7 +32,7 @@ class AggregatePgStatStatementsController extends Controller
                     $aggPgStatStatement->created_at = $date;
                     $aggPgStatStatement->server = gethostname();
                     if (!$aggPgStatStatement->save()) {
-                        throw new \Exception(strlen(gethostname()));
+                        throw new \Exception(gethostname());
                     }
                 }
             }
